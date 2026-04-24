a6358fcc9619f2926fb759e21472d89e6699ec5f
Kim Ngan <ngandc24v7k043@vlvh.ctu.edu.vn>
2026-04-24 16:27:21 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/services/contact.service.js
@@ -0,0 +1,23 @@
+class ContactService {
+    constructor(baseUR = "/api/contacts") {
+        this.api = createApiClient(baseUR);
+    }
+
+    async getAll() {
+        return (await this.api.get("/")).data;
+    }
+
+    async create(data) {
+        return (await this.api.post("/", data)).data;
+    }
+
+    async deleteAll() {
+        return (await this.api.delete("/")).data;
+    }
+
+    async get(id) {
+        return (await this.api.get(`/${id}`)).data;
+    }
+}
+
+export default new ContactService();
