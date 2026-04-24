b86896bc3150a9f481b7028926cc8c2fb497b4be
Kim Ngan <ngandc24v7k043@vlvh.ctu.edu.vn>
2026-04-24 17:21:08 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -33,6 +33,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span
+          >
+        </router-link>
       </div>
     </div>
   </div>
@@ -104,7 +114,7 @@ export default {
         }
       }
     },
-    
+
     goToAddContact() {
       this.$router.push({ name: "contact.add" });
     },
